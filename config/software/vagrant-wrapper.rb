e1d1f6331240d132240a832bbd60bde333a1eec9
Kent R. Spillner <kspillner@acm.org>
2014-03-12 12:03:48 -0500

    Fix vagrant-wrapper after Hashicorp changes
    Hashicorp recently deleted their puppet-modules repository which
    we were dependent on for the vagrant bin script that correctly
    sets up the necessary environment variables for Vagrant and executes
    the executable from the gem.  As a quick work-around simply pull a
    recent copy of that script, make it available in a new repository
    (also called vagrant-wrapper) and switch to using that.
    
    I compared the wrapper script with the current official way of
    packaging Vagrant to ensure the script will continue to work with
    newer releases.  Whenever the Vagrant team settles on a final
    packaging system we can consider replacing this wrapper script
    with something based on upstream's work, but in the meantime I
    don't anticipate any major difficulties keeping this approach
    working.

--- config/software/vagrant-wrapper.rb
+++ config/software/vagrant-wrapper.rb
@@ -1,12 +1,11 @@
 name "vagrant-wrapper"
 version "master"
 
-source :git => "git://github.com/hashicorp/puppet-modules"
+source :git => "git://github.com/sl4mmy/vagrant-wrapper"
 
 relative_path "vagrant-wrapper"
 
 build do
-  command 'sed -i "/^export GEM_PATH=/ s/\/gems/\/lib\/ruby\/gems\/1\.9\.1\//" modules/vagrant_installer/templates/vagrant.erb'
-  command "cp modules/vagrant_installer/templates/vagrant.erb #{install_dir}/bin/vagrant"
+  command "cp bin/vagrant #{install_dir}/bin/vagrant"
   command "chmod +x #{install_dir}/bin/vagrant"
 end
